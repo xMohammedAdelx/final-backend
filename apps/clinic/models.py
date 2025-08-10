42fa2dab630f9fa3b0b0d1577a42243a5f25e18b
Youssef Osama <osama74454@gmail.com>
2025-08-10 15:40:23 +0300

    upload project structure

---
+++ apps/clinic/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+# Create your models here.
+
+# Clinic Profile
+class ClinicProfile(models.Modal):
+    pass
+
+
+# Clinic Service
+class ClinicService(models.Modal):
+    pass
+
+
+# Clinic Doctors
+class ClinicDoctors(models.Modal):
+    pass
+
+
+# Clinic Gallary
+class ClinicGallary(models.Modal):
+    pass
+
+
+# Clinic Review
+class ClinicReview(models.Modal):
+    pass
+
+
+# Clinic Appointment
+class ClinicAppointment(models.Modal):
+    pass
+
+
+# Clinic Working Hours
+class ClinicWorkingHours(models.Modal):
+    pass
+
+
+# Clinic Contact Message
+class ClinicContactMessage(models.Modal):
+    pass
---
+++ apps/portofolio/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+# Create your models here.
+
+
+# Dentist Profile
+class DentistProfile(models.Model):
+    pass
+
+
+# Skill
+class Skill(models.Model):
+    pass
+
+
+# Education
+class Education(models.Model):
+    pass
+
+
+# Experiance
+class Experiance(models.Model):
+    pass
+
+
+# Project
+class Project(models.Model):
+    pass
+
+
+# Testimonial
+class Testimonial(models.Model):
+    pass
+
+# FAQ
+class FAQ(models.Modal):
+    pass
+
+
+# Awards
+class Awards(models.Modal):
+    pass
+
+
+# Certification
+class Certification(model.Modal):
+    pass
+
+
+# Service
+class Service(model.Modal):
+    pass
+
+
